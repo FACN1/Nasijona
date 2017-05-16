12134323dd7d434be2670090e69885c58ea4df35
JWLD <jackwldavies@icloud.com>
2017-05-16 12:26:07 +0300

    remove e from listener function under duress

--- prototype/2_register/index.js
+++ prototype/2_register/index.js
@@ -1,5 +1,5 @@
 // save name and email to local storage on form submit
-document.getElementById('submit').addEventListener('click', function(e) {
+document.getElementById('submit').addEventListener('click', function() {
   var formInputs = document.getElementById('form').elements;
   localStorage.sellerName = formInputs.name.value;
   localStorage.sellerEmail = formInputs.email.value;
