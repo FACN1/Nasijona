036086d304a5b40b9c1ede97e72911a7e70e4b4e
JWLD <jackwldavies@icloud.com>
2017-05-31 11:38:07 +0300

    add bcrypt password hashing #75

--- src/routes/add_user.js
+++ src/routes/add_user.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const dbQueries = require('../db_queries.js');
+const bcrypt = require('bcrypt');
 
 module.exports = (req, res) => {
   MongoClient.connect(process.env.DATABASE_URL, (err, db) => {
@@ -8,7 +9,7 @@ module.exports = (req, res) => {
     const data = {
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password
+      password: bcrypt.hashSync(req.body.password, 10)
     };
 
     return dbQueries.addUser(db, data, (error) => {
