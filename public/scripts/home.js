5f6b3b0b87556872dfdf108c6a2ee052397afff6
JWLD <jackwldavies@icloud.com>
2017-06-03 21:06:17 +0300

    change how login button determines logged in status #77

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,7 +1,12 @@
 (function() {
   document.getElementById('login-button').addEventListener('click', function(e) {
+    // search for token cookie
+    var token = document.cookie.split(';').filter(function(cookie) {
+      return cookie.trim().split('=')[0] === 'token';
+    });
+
     // redirect to login, or delete cookie and reload
-    if (e.target.innerHTML === 'LOG IN') {
+    if (token.length === 0) {
       location.href = '/login';
     } else {
       document.cookie = 'token=; expires=Thu 01 Jan 1970 00:00:00 UTC;';
--- src/routes/validate.js
+++ src/routes/validate.js
@@ -1,4 +1,3 @@
 module.exports = (req, res, next) => {
-  console.log(req.cookies);
   return next();
 };
