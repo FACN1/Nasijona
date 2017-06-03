201c929f48e2419ebf0d09df1caf39a22644b843
JWLD <jackwldavies@icloud.com>
2017-06-03 18:45:48 +0300

    create login route and form #46

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.get('/', require('./home.js'));
 router.get('/register', require('./register.js'));
+router.get('/login', require('./login.js'));
 router.get('/profile', require('./profile.js'));
 
 router.post('/add-user', require('./add_user.js'));
