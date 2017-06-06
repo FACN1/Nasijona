aff3f068094f02d555644a8f799904b12c3acdd6
JWLD <jackwldavies@icloud.com>
2017-06-06 08:23:55 +0300

    handle errors if profile in url not found

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -4,6 +4,10 @@ module.exports = (req, res) => {
   dbQueries.getProfile(req.query.u, (err, cursor) => {
     if (err) return res.status(500).send('Database error.');
 
+    if (!cursor) {
+      return res.status(404).send('Profile not found.');
+    }
+
     return res.render('profile', {
       title: cursor.shop,
       about: cursor.about,
