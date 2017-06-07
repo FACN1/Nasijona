fcd78086e1a17ce4784ad45975361a5797017637
JWLD <jackwldavies@icloud.com>
2017-06-07 09:50:21 +0300

    tweak add product form, add image upload #53

--- public/scripts/new_product.js
+++ public/scripts/new_product.js
@@ -1,3 +1,7 @@
 (function() {
-  console.log(123);
+  // update char count for description input
+  document.getElementById('desc-text').addEventListener('input', function(e) {
+    var count = e.target.value.length;
+    document.getElementById('desc-char-count').textContent = 100 - count;
+  });
 })();
